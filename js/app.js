74dd9d0ac0778666e5b50b617518e7842455fbdc
rickjp <3741326+RickJP@users.noreply.github.com>
2019-10-10 23:47:43 +0900

    Removed More unnecessary elements

--- js/app.js
+++ js/app.js
@@ -14,73 +14,38 @@ var audioContext; //new audio context to help us record
 //var encodingTypeSelect = document.getElementById("encodingTypeSelect");
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
+var uploadButton = document.getElementById("uploadButton");
+uploadButton.style.visibility = "hidden";
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 
+
 function startRecording() {
 	console.log("startRecording() called");
 
-	/*
-		Simple constraints object, for more advanced features see
-		https://addpipe.com/blog/audio-constraints-getusermedia/
-	*/
-    
     var constraints = { audio: true, video:false }
 
-    /*
-    	We're using the standard promise based getUserMedia() 
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-
 	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		//__log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
-
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-
-		*/
+		
 		audioContext = new AudioContext();
-
-		//update the format 
-		//document.getElementById("formats").innerHTML="Format: 2 channel "+encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value+" @ "+audioContext.sampleRate/1000+"kHz"
-
-		//assign to gumStream for later use
 		gumStream = stream;
-		
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
-		
-		//stop the input from playing back through the speakers
-		//input.connect(audioContext.destination)
 
-		//get the encoding 
-		//encodingType = encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value;
-		
-		//disable the encoding selector
-		//encodingTypeSelect.disabled = true;
+		input = audioContext.createMediaStreamSource(stream);
 
 		recorder = new WebAudioRecorder(input, {
 		  workerDir: "js/", // must end with slash
 		  encoding: "mp3",
 		  numChannels:2, //2 is the default, mp3 encoding supports only 2
-		  onEncoderLoading: function(recorder, encoding) {
-		    // show "loading encoder..." display
-		    //__log("Loading "+encoding+" encoder...");
+		  onEncoderLoading: function(recorder, encoding) {  
 		  },
 		  onEncoderLoaded: function(recorder, encoding) {
-		    // hide "loading encoder..." display
-		    //__log(encoding+" encoder loaded");
 		  }
 		});
 
 		recorder.onComplete = function(recorder, blob) { 
-			//__log("Encoding complete");
-			createDownloadLink(blob,recorder.encoding);
-			//encodingTypeSelect.disabled = false;
+			createUploadLink(blob);
 		}
 
 		recorder.setOptions({
@@ -121,31 +86,67 @@ function stopRecording() {
 	recorder.finishRecording();
 
 	//__log('Recording stopped');
+
 }
 
-function createDownloadLink(blob,encoding) {
-	console.log('Create DL Link');
+
+function createUploadLink(blob) {
+	
 	var url = URL.createObjectURL(blob);
 	var au = document.createElement('audio');
 	var li = document.createElement('li');
 	var link = document.createElement('a');
- 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
 
-	//link the a element to the blob
-	link.href = url;
-	link.download = new Date().toISOString() + '.'+encoding;
-	link.innerHTML = link.download;
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
 
-	//add the new audio and a elements to the li element
-	li.appendChild(au);
-	li.appendChild(link);
+	//upload link
+	//var upload = document.createElement('a');
+  uploadButton.href="#";
+  uploadButton.innerHTML = "UPLOAD"
+  
+	uploadButton.addEventListener("click", function(event){
+    console.log('clicked');
+		  var xhr=new XMLHttpRequest();
+		  xhr.onload=function(e) {
+		      if(this.readyState === 4) {
+		          console.log("Server returned: ",e.target.responseText);
+		      }
+		  };
+		  var fd=new FormData();
+		  fd.append("audio_data",blob, filename);
+		  xhr.open("POST","upload.php",true);
+		  xhr.send(fd);
+	});
 
-	//add the li element to the ordered list
-	//recordingsList.appendChild(li);
+  uploadButton.style.visibility = "visible";
+
 }
+
+
+// function createDownloadLink(blob,encoding) {
+// 	console.log('Create DL Link');
+// 	var url = URL.createObjectURL(blob);
+// 	var au = document.createElement('audio');
+// 	var li = document.createElement('li');
+// 	var link = document.createElement('a');
+ 
+// 	//add controls to the <audio> element
+// 	au.controls = true;
+// 	au.src = url;
+
+// 	//link the a element to the blob
+// 	link.href = url;
+// 	link.download = new Date().toISOString() + '.'+encoding;
+// 	link.innerHTML = link.download;
+
+// 	//add the new audio and a elements to the li element
+// 	li.appendChild(au);
+// 	li.appendChild(link);
+
+// 	//add the li element to the ordered list
+// 	//recordingsList.appendChild(li);
+// }
 
 
 
